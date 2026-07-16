49e2a4b2f433f69bec8e20947637e2a93cdc4344
apid01 <apurvadhakre281@gmail.com>
2026-07-17 01:46:14 +0530

    feat(minispring): implement basic IoC container

--- src/main/java/com/apurva/taskflow/minispring/context/TFApplication.java
+++ src/main/java/com/apurva/taskflow/minispring/context/TFApplication.java
@@ -1,12 +1,27 @@
 package com.apurva.taskflow.minispring.context;
 import com.apurva.taskflow.minispring.annotation.TFService;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TFApplication {
-    public static void run(Class<?>... classes) {
+    private static final Map<Class<?>, Object> beans = new HashMap<>();
+    public static void run(Class<?>... classes) throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+
         for(Class<?> clazz : classes){
             if(clazz.isAnnotationPresent(TFService.class)){
-                System.out.println("Found TF Service : "+ clazz.getSimpleName());
+                Constructor<?> constructor = clazz.getDeclaredConstructor();
+                Object bean = constructor.newInstance();
+                beans.put(clazz, bean);
+                System.out.println("Registered Bean : " + clazz.getSimpleName());
+                System.out.println(beans);
             }
         }
+
+    }
+    public static Object getBean(Class<?> clazz) {
+        return beans.get(clazz);
     }
 }
--- src/main/java/com/apurva/taskflow/minispring/demo/MiniSpringDemo.java
+++ src/main/java/com/apurva/taskflow/minispring/demo/MiniSpringDemo.java
@@ -3,8 +3,14 @@ package com.apurva.taskflow.minispring.demo;
 import com.apurva.taskflow.minispring.context.TFApplication;
 import com.apurva.taskflow.service.UserService;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class MiniSpringDemo {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
         TFApplication.run(UserService.class);
+        UserService service =
+                (UserService) TFApplication.getBean(UserService.class);
+
+        System.out.println(service);
     }
 }
